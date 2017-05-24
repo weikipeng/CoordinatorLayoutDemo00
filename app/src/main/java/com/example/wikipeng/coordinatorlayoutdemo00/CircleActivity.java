4037eb5a666ff5b83c3ddabd64c86f8b42fcf108
wikipeng <pengjunwei@100tal.com>
2017-05-24 20:21:02 +0800

    实现伸缩标题栏

--- app/src/main/java/com/example/wikipeng/coordinatorlayoutdemo00/CircleActivity.java
+++ app/src/main/java/com/example/wikipeng/coordinatorlayoutdemo00/CircleActivity.java
@@ -2,17 +2,28 @@ package com.example.wikipeng.coordinatorlayoutdemo00;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by wikipeng on 2017/5/24.
  */
 
 public class CircleActivity extends AppCompatActivity{
-
+    protected Toolbar mToolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setTitle("");
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        //        getSupportActionBar().setTitle("");
+//        mToolbar.setTitle("测试");
+//        mToolbar.setSubtitle("subTitle");
     }
 }
