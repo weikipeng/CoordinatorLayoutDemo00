447704b2d97c3c41ceb21c7e71d2a194a2b41f0a
wikipeng <pengjunwei@100tal.com>
2017-05-24 19:42:00 +0800

    模仿圈子页面

---
+++ app/src/main/java/com/example/wikipeng/coordinatorlayoutdemo00/CircleActivity.java
@@ -0,0 +1,18 @@
+package com.example.wikipeng.coordinatorlayoutdemo00;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wikipeng on 2017/5/24.
+ */
+
+public class CircleActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+    }
+}
